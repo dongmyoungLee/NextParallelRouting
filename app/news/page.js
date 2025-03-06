6d1ec7d000bf1fc2faaed9d2f76dcda867226131
dongmyounglee <pajang1515@daum.net>
2025-03-06 14:31:33 +0900

    archive directory 병렬 라우팅 작업

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,11 @@
-import Link from "next/link";
+import NewsList from "@/components/news-list";
 import {DUMMY_NEWS} from "@/dummy-news";
-import Image from "next/image";
 
 export default function NewsPage() {
     return (
         <>
             <h1>news</h1>
-            <ul className="news-list">
-                {DUMMY_NEWS.map(newItem => <li key={newItem.id}>
-                    <Link href={`/news/${newItem.slug}`}>
-                        <img src={`/images/news/${newItem.image}`} alt={newItem.title} />
-                        <span>{newItem.title}</span>
-                    </Link>
-                </li>)}
-            </ul>
+            <NewsList news={DUMMY_NEWS} />
         </>
 
     );
---
+++ components/news-list.js
@@ -0,0 +1,15 @@
+import {DUMMY_NEWS} from "@/dummy-news";
+import Link from "next/link";
+
+export default function NewsList({ news }) {
+    return (
+        <ul className="news-list">
+            {news.map(newItem => <li key={newItem.id}>
+                <Link href={`/news/${newItem.slug}`}>
+                    <img src={`/images/news/${newItem.image}`} alt={newItem.title} />
+                    <span>{newItem.title}</span>
+                </Link>
+            </li>)}
+        </ul>
+    );
+}
